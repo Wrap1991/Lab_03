5c1763404fd96d99aa117eab594e475fee6be80b
Wrap1991 <wrapdos@gmail.com>
2023-04-27 15:51:55 -0500

    wrap

---
+++ ejercicio1.js
@@ -0,0 +1,218 @@
+var nombre = "Carlos";
+console.log(nombre);
+
+var numero = 1000;
+console.log(numero);
+
+var objeto = {
+    nombre : "Juan",
+    apellido : "Perez",
+    telefono : "55443322"
+}
+console.log(objeto);
+
+
+var bandera = false;
+console.log(typeof bandera);
+
+function miFuncion(){}
+console.log(typeof miFuncion);
+
+var simbolo = Symbol("mi simbolo");
+console.log(typeof simbolo);
+
+class Persona{
+    constructor(nombre, apwllido9){
+        this.nombre = nombre;
+        this.apellido =apellido;
+    }
+}
+console.log(typeof Persona);
+
+var x;
+console.log(typeof x);
+
+x = undefined;
+console.log(typeof x);
+
+var y = null;
+console.log(typeof y);
+
+
+
+
+
+
+var y = null;
+console.log(typeof y);
+
+var autos = ['BMW','Audit','Volvo'];
+console.log(autos);
+console.log(typeof autos);
+
+var z = '';
+console.log(z);
+console.log(typeof z);
+
+
+
+
+
+
+
+
+var nombre = 'Juan';
+var apellido = 'Perez';
+
+var nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
+
+var nombreCompleto2 = 'Carlos' + ' ' +'Lara';
+console.log(nombreCompleto2);
+
+x = nombre + 2 + 4;
+console.log(x);
+
+x = 2 + 4 + nombre;
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+let nombre;
+nombre = "Juan";
+console.log( nombre );
+
+const apellido = "Perez";
+
+
+
+
+
+
+let nombreCompleto = "Juan Perez";
+let nombreCompleto = "Carlos Lara";
+console.log( nombreCompleto );
+console.log( nombreCompleto );
+
+let alnombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+
+let ruptura = 10;
+
+
+
+
+//let a = 3, b = 2;
+//let z = a + b;
+//console.log("Resultado de la suma : " + z);
+
+//z = a - b;
+//console.log("Resultado de la resta : " + z);
+
+//z = a * b;
+//console.log("Resultado de multi : " + z);
+
+//z = a / b;
+//console.log("Resultado de la division : " + z);
+
+//z = a % b;
+//console.log("Resultado de operacion modulo (residuo): " + z);
+
+//z = a ** b;
+//console.log("Resultado de operador exponente: " + z);
+
+
+
+
+
+
+
+//let a = 3, b = 2;
+//let z = a + b;
+
+//z = ++a;
+//console.log(a);
+//console.log(z);
+
+//z = b++;
+//console.log(b);
+//console.log(z);
+
+//z = --a;
+//console.log(a);
+//console.log(z);
+
+//z = b--;
+//console.log(b);
+//console.log(z);
+
+
+
+
+
+
+
+//let a = 3, b = 2, c = 1, d = 4;
+
+//let z = a * b + c / d;
+//console.log(z);
+
+//z = (c + a) * b / c;
+//console.log(z);
+
+
+
+
+
+
+//let a = 3, b = 2, c = "3";
+
+//let z = a == c;
+//console.log(z);
+
+//z = a === c;
+//console.log(z);
+
+
+
+
+
+
+//let a = 3, b = 2, c = 3;
+
+//let z = a != c;
+//console.log(z);
+
+//z = a !== c;
+//console.log(z);
+
+
+
+
+
+
+let a = 15;
+let valMin = 0, valMax = 10;
+
+if( a >= valMin && a <= valMax){
+    console.log("Dentro de rango");
+}
+else{
+    console.log("Fuera de rango");
+}
+
+
+
+
+
+
+
+
---
+++ jsd-ejercicio6.js
@@ -0,0 +1,10 @@
+let nombreCompletos = "Juan Perez";
+let nombreCompleto = "Carlos Lara";
+console.log( nombreCompleto );
+console.log( nombreCompleto );
+
+let alnombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+
+let ruptura = 10;
